12847e7a61361119c10deca39941dc2da34b0bf9
Andrés Urmeneta <andresub009@gmail.com>
2020-07-29 21:53:05 -0400

    CORS implementado a tráves de proxy * Búsquedas son redirecccionadas a cors-anywhere para tener respuesta con headers de CORS. Esto solociona #1. * Dependencia axios es sustituida por node-fetch

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020, Andrés Urmeneta B. <aurmenetab@gmail.com>
+Copyright (c) 2020, Andrés Urmeneta B. <aurmeneta@uc.cl>
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
 copyright notice and this permission notice appear in all copies.
@@ -12,65 +12,73 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
 */
 
-const axios = require("axios");
+const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
-const url_base = "http://buscacursos.uc.cl/?";
+const fetch_config = {
+    headers: {
+        'Origin': 'aurmeneta/buscacursos-uc'
+    }
+}
+
+const url_base = "https://cors-anywhere.herokuapp.com/http://buscacursos.uc.cl/?";
 
 function obtenerCursos(url) {
-    return axios.get(url).then(response => {
-        let cursos = []; // Arreglo para guardar los cursos encontrados.
-        let filas = []; // Arreglo para guardar las filas obtenidas desde la tabla html.
+    return fetch(url, fetch_config)
+        .then(response => response.text())
+        .then(html => {
+            let cursos = []; // Arreglo para guardar los cursos encontrados.
+            let filas = []; // Arreglo para guardar las filas obtenidas desde la tabla html.
 
-        // Carga la pagina web en el selector.
-        const $ = cheerio.load(response.data);
+            // Carga la pagina web en el selector.
+            const $ = cheerio.load(html);
 
-        // Busca las filas de la tabla.
-        filas = $('.resultadosRowPar').toArray();
-        filas = filas.concat($('.resultadosRowImpar').toArray());
+            // Busca las filas de la tabla.
+            filas = $('.resultadosRowPar').toArray();
+            filas = filas.concat($('.resultadosRowImpar').toArray());
 
-        // Convierte las filas html en objetos.
-        filas.forEach(fila => {
+            // Convierte las filas html en objetos.
+            filas.forEach(fila => {
 
-            let columnas = $(fila).find('td');
+                let columnas = $(fila).find('td');
 
-            let nrc = $(columnas[0]).text().trim();
-            let sigla = $(columnas[1]).text().trim();
-            let seccion = parseInt($(columnas[4]).text());
-            let nombre = $(columnas[9]).text().trim();
-            let profesor = $(columnas[10]).text()
-                .split(',')
-                .map(texto => texto.trim());
-            let vacantes_disponibles = parseInt($(columnas[14]).text());
+                let nrc = $(columnas[0]).text().trim();
+                let sigla = $(columnas[1]).text().trim();
+                let seccion = parseInt($(columnas[4]).text());
+                let nombre = $(columnas[9]).text().trim();
+                let profesor = $(columnas[10]).text()
+                    .split(',')
+                    .map(texto => texto.trim());
+                let vacantes_disponibles = parseInt($(columnas[14]).text());
 
-            let horario = [];
-            
-            let filas_horario = $(columnas[16]).find('tr').toArray();
-            
-            filas_horario.forEach(fila_horario => {
-                let columnas_horario = $(fila_horario).find('td');
+                let horario = [];
 
-                let tipo = $(columnas_horario[1]).text().trim();
-                let sala = $(columnas_horario[2]).text().trim();
+                let filas_horario = $(columnas[16]).find('tr').toArray();
 
-                let horas_dias = $(columnas_horario[0]).text().trim().split(':');
-                let dias = horas_dias[0].split('-');
-                let horas = horas_dias[1].split(',').map(hora => parseInt(hora));
+                filas_horario.forEach(fila_horario => {
+                    let columnas_horario = $(fila_horario).find('td');
 
-                dias.forEach(dia => {
-                    horas.forEach(hora => {
-                        horario.push({tipo, dia, hora, sala})
+                    let tipo = $(columnas_horario[1]).text().trim();
+                    let sala = $(columnas_horario[2]).text().trim();
+
+                    let horas_dias = $(columnas_horario[0]).text().trim().split(':');
+                    let dias = horas_dias[0].split('-');
+                    let horas = horas_dias[1].split(',').map(hora => parseInt(hora));
+
+                    dias.forEach(dia => {
+                        horas.forEach(hora => {
+                            horario.push({tipo, dia, hora, sala})
+                        });
                     });
                 });
+
+
+
+                cursos.push({nrc, sigla, seccion, nombre, profesor, vacantes_disponibles, horario})
             });
 
-
-
-            cursos.push({nrc, sigla, seccion, nombre, profesor, vacantes_disponibles, horario})
+            return cursos;
         });
-
-        return cursos;
-    });
 }
 
 function buscarSigla(periodo, sigla) {
