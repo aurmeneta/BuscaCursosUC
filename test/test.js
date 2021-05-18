3c3a4cd214bb675b8a567ef7006e0deab5fc2243
Andrés Urmeneta <andresub009@gmail.com>
2021-05-17 20:39:58 -0400

    choques

--- test/test.js
+++ test/test.js
@@ -1,24 +1,28 @@
 const buscacursos = require("../index.js");
+const { Curso, Modulo } = buscacursos;
 
-let obtenerCursos = buscacursos.obtenerCursos("http://buscacursos.uc.cl/?cxml_semestre=2020-1&cxml_sigla=mat1620");
+const periodo = "2021-1"
 
-let buscarSigla = buscacursos.buscarSigla("2020-1", "mat1023");
+let obtenerCursos = buscacursos.obtenerCursos("http://buscacursos.uc.cl/?cxml_semestre=2020-2&cxml_sigla=mat1620");
 
-let buscarProfesor = buscacursos.buscarProfesor("2020-1", "marta garcia-huidobro");
+let buscarSigla = buscacursos.buscarSigla(periodo, "mat1023");
 
-let buscarCurso = buscacursos.buscarCurso("2020-1", "calculo ii");
+let buscarProfesor = buscacursos.buscarProfesor(periodo, "marta garcia-huidobro");
 
-let probarMuchosHorarios = buscacursos.buscarSigla("2020-1", "AQT0000");
+let buscarCurso = buscacursos.buscarCurso(periodo, "calculo ii");
 
-let probarSinHorario = buscacursos.buscarSigla("2020-1", "ING1001");
+let probarMuchosHorarios = buscacursos.buscarSigla(periodo, "AQT0000");
+
+let probarSinHorario = buscacursos.buscarSigla(periodo, "ING1001");
 
 Promise.all([obtenerCursos, buscarSigla, buscarProfesor, buscarCurso, probarMuchosHorarios, probarSinHorario]).then(resultados => {
     resultados.forEach(cursos => console.log(cursos.length));
 
-    console.log(resultados[0][0])
-    console.log("\nMuchos Horarios")
+    console.log(resultados[0][0]);
+
+    console.log("\nMuchos Horarios");
     console.log(resultados[4][0].horario);
 
-    console.log("\nSin Horario")
+    console.log("\nSin Horario");
     console.log(resultados[5][0].horario);
-})
+});
