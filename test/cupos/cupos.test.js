5de6136d7eb884fb063abc9811109487f2ab136e
Andrés Urmeneta Betzhold <aurmeneta@uc.cl>
2024-01-06 15:14:09 +0100

    v2.2.0 (#13)
    * chore: deps updated
    
    * chore: cheerio actualizado
    
    * feat: scraping de todas las columnas de cupos
    
    * chore: updated tests
    
    * chore: updated deps
    
    * fix: rollback to node-fetch ^2

--- test/cupos/cupos.test.js
+++ test/cupos/cupos.test.js
@@ -2,7 +2,7 @@
 const { cupos } = require('../../index')
 
 test('obtener cupos retorna resultados no vacíos', async () => {
-  const resultado = await cupos.obtenerCupos('2022-1', '14833')
+  const resultado = await cupos.obtenerCupos('2024-1', '14833')
 
   expect(resultado.nrc).toBe('14833')
   expect(resultado.sigla).toBe('MAT1620-1')
@@ -11,8 +11,30 @@ test('obtener cupos retorna resultados no vacíos', async () => {
   expect(resultado.cupos.length).toBeGreaterThanOrEqual(1)
 })
 
+test('obtener cupos retorna cupos para distintas esculas, programas, concentraciones, admsiones, etc.', async () => {
+  const resultado = await cupos.obtenerCupos('2023-2', '12489')
+
+  expect(resultado.nrc).toBe('12489')
+  expect(resultado.sigla).toBe('ICS1113-1')
+  expect(resultado.vacantesDisponibles).not.toBeNaN()
+  expect(resultado.inseguro).toBe(false)
+  expect(resultado.cupos.length).toBeGreaterThanOrEqual(1)
+
+  for (cupo of resultado.cupos) {
+    expect(cupo.escuela).not.toBeUndefined()
+    expect(cupo.nivel).not.toBeUndefined()
+    expect(cupo.programa).not.toBeUndefined()
+    expect(cupo.concentracion).not.toBeUndefined()
+    expect(cupo.cohorte).not.toBeUndefined()
+    expect(cupo.admision).not.toBeUndefined()
+    expect(cupo.vacantesOfrecidas).not.toBeNaN()
+    expect(cupo.vacantesOcupadas).not.toBeNaN()
+    expect(cupo.vacantesDisponibles).not.toBeNaN()
+  }
+})
+
 test('obtener cupos de curso sin reservas retorna resultados no vacíos', async () => {
-  const resultado = await cupos.obtenerCupos('2022-1', '12745')
+  const resultado = await cupos.obtenerCupos('2024-1', '12745')
 
   expect(resultado.nrc).toBe('12745')
   expect(resultado.sigla).toBe('DPT6500')
--- test/cursos/cursos.test.js
+++ test/cursos/cursos.test.js
@@ -2,7 +2,7 @@
 const { cursos } = require('../../index')
 
 test('obtenerCurso obtiene datos correctamente', async () => {
-  const curso = await cursos.obtenerCurso('2022-1', 12745)
+  const curso = await cursos.obtenerCurso('2023-1', 12745)
   expect(curso).toEqual(
     new cursos.Curso(
       '12745',
@@ -18,8 +18,8 @@ test('obtenerCurso obtiene datos correctamente', async () => {
       ['Echeverría Francisca'],
       'San Joaquín',
       5,
-      20,
-      2,
+      21,
+      5,
       [
         new cursos.Modulo('CLAS', 'L', 1, 'C-TENIS'),
         new cursos.Modulo('CLAS', 'W', 1, 'C-TENIS'),
@@ -30,21 +30,21 @@ test('obtenerCurso obtiene datos correctamente', async () => {
 })
 
 test('obtenerCursos obtiene cantidad correcta de resultados', async () => {
-  const resultados = await cursos.obtenerCursos('https://buscacursos.uc.cl/?cxml_semestre=2022-1&cxml_sigla=MAT1620&cxml_nrc=&cxml_nombre=&cxml_categoria=TODOS&cxml_area_fg=TODOS&cxml_formato_cur=TODOS&cxml_profesor=&cxml_campus=TODOS&cxml_unidad_academica=TODOS&cxml_horario_tipo_busqueda=si_tenga&cxml_horario_tipo_busqueda_actividad=TODOS#resultados')
+  const resultados = await cursos.obtenerCursos('https://buscacursos.uc.cl/?cxml_semestre=2024-1&cxml_sigla=MAT1620&cxml_nrc=&cxml_nombre=&cxml_categoria=TODOS&cxml_area_fg=TODOS&cxml_formato_cur=TODOS&cxml_profesor=&cxml_campus=TODOS&cxml_unidad_academica=TODOS&cxml_horario_tipo_busqueda=si_tenga&cxml_horario_tipo_busqueda_actividad=TODOS#resultados')
   expect(resultados.length).toBe(8)
 })
 
 test('buscarSigla obtiene cantidad correcta de resultados', async () => {
-  const resultados = await cursos.buscarSigla('2022-1', 'MAT1620')
+  const resultados = await cursos.buscarSigla('2024-1', 'MAT1620')
   expect(resultados.length).toBe(8)
 })
 
 test('buscarCurso obtiene cantidad correcta de resultados', async () => {
-  const resultados = await cursos.buscarCurso('2022-1', 'Cálculo III')
+  const resultados = await cursos.buscarCurso('2024-1', 'Cálculo III')
   expect(resultados.length).toBe(11)
 })
 
 test('buscarProfesor obtiene cantidad correcta de resultados', async () => {
-  const resultados = await cursos.buscarProfesor('2022-1', 'Francisca Echeverría')
-  expect(resultados.length).toBe(11)
+  const resultados = await cursos.buscarProfesor('2024-1', 'Francisca Echeverría')
+  expect(resultados.length).toBe(9)
 })
